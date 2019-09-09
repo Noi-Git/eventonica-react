0660a093494953aae641bc0ff153c1ec5c48760a
Noi <noi.patsin@gmail.com>
2019-09-09 13:30:50 -0700

    add eslint-utils@1.4.1

--- eventonica-react-client/src/components/Header.js
+++ eventonica-react-client/src/components/Header.js
@@ -4,7 +4,14 @@ class Header extends Component {
   render() {
     return (
       <div>
-        Header
+      <nav>
+      <div className="nav-wrapper container">
+        <a href="#" className="brand-logo">Eventonica-React</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="sass.html">Login with Google</a></li>
+        </ul>
+      </div>
+    </nav>
       </div>
     )
   }
